089ecc63bf10afa6f2c08bd703d4771376b4941a
Jordon Roussie <jordonroussie96@gmail.com>
2020-02-17 21:33:46 -0400

    all homework up to 4 done.

--- 1 First Apps/1.2/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ 1 First Apps/1.2/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import java.util.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,16 +15,36 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countUp: Button = findViewById(R.id.Count_Up)
+        countUp.setOnClickListener { CountUp() }
     }
 
     private fun rollDice() {
-        //Toast.makeText(this, "button clicked"
-            //Toast.LENGTH_SHORT).show()
-        val randomInt = Random().nextInt(6) + 1
+        val randomInt = (1..6).random()
+        //Toast.makeText(this, "button clicked",
+        //Toast.LENGTH_SHORT).show()
         val resultText: TextView = findViewById(R.id.result_text)
-        //resultText.text = "Dice Rolled!"
         resultText.text = randomInt.toString()
     }
 
-}
+    private fun CountUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
 
+        // If text is the default "Hello World!" set that text to 1.
+        if (resultText.text == "Hello World!") {
+            resultText.text = "1"
+        } else {
+            // Otherwise, increment the number up to 6.
+            // The text value in resultText.text is an instance of the CharSequence class;
+            // it needs to be converted to a String object before it can be converted to an int.
+            var resultInt = resultText.text.toString().toInt()
+
+            if (resultInt < 6) {
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
+
+    }
+}
